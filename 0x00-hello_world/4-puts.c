8c86505bbe9e2a97b17977104d311ee95e528a5e
Mass-V <84704932+Mass-V@users.noreply.github.com>
2021-08-26 06:01:41 -0700

    c code puts and printf functions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
-/* The puts function reads and prints to <stdout> aka screen */
+/* the puts reads and prints to <stdout> aka screen */
 
 #include <stdio.h>
 
 /**
- * main - prints "Programming is a multilingual puzzle"
- *
- * Return: 0 coz main is void
+* main - prints "Programming is like building a multilingual puzzle"
+*
+* Return: always 0 when main is set to void for int.
 */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
