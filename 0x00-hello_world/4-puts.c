698710d59c9fbe1d28e1dca4778e684e2d66ff10
Mass-V <84704932+Mass-V@users.noreply.github.com>
2021-08-26 05:34:27 -0700

    my 1st c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/* The puts function reads and prints to <stdout> aka screen */
+
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is a multilingual puzzle"
+ *
+ * Return: 0 coz main is void
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
