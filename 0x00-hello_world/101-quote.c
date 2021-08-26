b47838cc7325dc50de79fc7134f081e2a4942013
Mass-V <84704932+Mass-V@users.noreply.github.com>
2021-08-26 09:26:30 -0700

    last trial again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
 /* printing to standard error */
 
 #include <unistd.h>
-#include <stdio.h>
 
 /**
  * main - using fwrite to print out to stderr the words below
@@ -10,8 +9,9 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	       39, 2, stderr);
+	write(2,
+	       "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	       59);
 
 	return (1);
 }
