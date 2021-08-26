d26c368de38f217baa0e3d1549abd406c73e9ed3
Mass-V <84704932+Mass-V@users.noreply.github.com>
2021-08-26 08:39:25 -0700

    assembly code <intel syntax> and program that prints to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+/* printing to standard error */
+
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - using fwrite to print out to stderr the words "
+*and that piece of art is usefull" - Dora Korpar, 2015-10-19 "
+ *
+ * Return: this time 1
+ */
+int main(void)
+{
+	fwrite("and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n",
+	       39, 1, stderr);
+
+	return (1);
+}
