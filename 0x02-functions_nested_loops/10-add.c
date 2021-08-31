03906c81c4a592a1b3af2ec462abca6edd2638ca
Mass-V <84704932+Mass-V@users.noreply.github.com>
2021-08-31 12:40:10 -0700

    some commits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - a function that adds two integers and returns the result
+ * @one: number input
+ * @two: number input
+ * Return: sum of the two integers
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
